b4d7329df0d2d81beab0655a4c6b8013f9f23e76
Pinku <pinkuparmer96@gmail.com>
2024-07-04 14:13:52 +0530

    update

--- My_App/src/App.jsx
+++ My_App/src/App.jsx
@@ -12,9 +12,9 @@ function App() {
     <p>The following is a  list of all the topics we can cover in the MDN learning area.</p>
     <a href=""><u>Getting started with the web</u></a>
     <p>Provides a practical introduction to web development for complete beginners.</p>
-    <a href="">HTML - Structuring the web</a>
+    <a href=""><u>HTML - Structuring the web</u></a>
     <p>HTML is the language what we use to structure the different parts of our content <br/>and define what their meaning or purpose is.This topic teaches HTML in detail.</p>
-    <a href="">CSS - Styling the web</a>
+    <a href=""><u>CSS - Styling the web</u></a>
     <p>CSS is the language that we use to control our web content's style and layout,as <br />well as adding behaviorlike annimation. This topic provides comprehensive <br />coverage of CSS.</p>
     </>
   )
